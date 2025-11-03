22ac9b7f476b3f97ff8311aa5c2fc94a0c0bbd3e
Ishidawg <willianscagol@gmail.com>
2025-11-03 19:12:02 -0300

    adjust for Factory Design Pattern

--- gui.py
+++ gui.py
@@ -1,11 +1,40 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QHBoxLayout, QVBoxLayout, QFileDialog, QLineEdit, QPushButton, QRadioButton
 from PySide6.QtCore import Qt
 
-class MainWindow(QMainWindow):
+class TitleLabel(QLabel):
+  def __init__(self, text):
+    super().__init__(f"<font size=6>{text}</font>")
+    self.setAlignment(Qt.AlignCenter)
+    self.setContentsMargins(0, 20, 0, 0)
 
+class SubTitleLabel(QLabel):
+  def __init__(self, text):
+    super().__init__(f"<font size=5>{text}</font>")
+    self.setAlignment(Qt.AlignLeft)
+    self.setContentsMargins(0, 20, 0, -18)
+
+class BodyLabel(QLabel):
+  def __init__(self, text):
+    super().__init__(f"{text}")
+    self.setAlignment(Qt.AlignJustify)
+    self.setWordWrap(True)
+
+class LabelFactory():
+  def createLabel(self, label_type, text):
+    match label_type:
+      case "title":       return TitleLabel(text)
+      case "sub_title":   return SubTitleLabel(text)
+      case "body":        return BodyLabel(text)
+
+    return print("You did not use a valid label_type")
+
+
+class MainWindow(QMainWindow):
 
   def __init__(self):
     super().__init__()
+
+    labelFactory = LabelFactory()
 
     WINDOW_WIDTH = 620
 
@@ -29,19 +58,10 @@ class MainWindow(QMainWindow):
     rendering_layout = QHBoxLayout(rendering_container)
     rendering_layout.setAlignment(Qt.AlignCenter)
 
-    # Main text stuff
-    label1 = QLabel("<font size=5>Reshade installer for proton games!<font>")
-    label1.setAlignment(Qt.AlignCenter)
-
-    label2 = QLabel("This is a <i>university project</i> that kinda works! The porpuse is to make reshade installation a bit easier. You just need to select the <b>root directory</b> <i>(where the .exe is)</i>, the application architecture and the rendering API. <font color=#ee2c2c><b>This is only intended for proton games.</b></font>")
-    label2.setAlignment(Qt.AlignJustify)
-    label2.setMaximumWidth(WINDOW_WIDTH)
-    label2.setWordWrap(True)
-
-
-    label3 = QLabel("<font size=5>Select the game directory</font>")
-    label3.setAlignment(Qt.AlignLeft)
-    label3.setContentsMargins(0, 20, 0, -40) # (left: int, top: int, right: int, bottom: int)
+    # Main label stuff
+    label1 = labelFactory.createLabel("title", "Reshade installer for proton games!")
+    label2 = labelFactory.createLabel("body", "This is a <i>university project</i> that kinda works! The porpuse is to make reshade installation a bit easier. You just need to select the <b>root directory</b> <i>(where the .exe is)</i>, the application architecture and the rendering API. <font color=#ee2c2c><b>This is only intended for proton games.</b></font>")
+    label3 = labelFactory.createLabel("sub_title", "Select the game directory")
 
     # Browse stuff
     line_edit = QLineEdit()
@@ -52,30 +72,19 @@ class MainWindow(QMainWindow):
     # file_dialog = QFileDialog()
 
     # Game architecture
-    label4 = QLabel("<font size=5>Select the game architecture</font>")
-    label4.setAlignment(Qt.AlignLeft)
-    label4.setContentsMargins(0, 20, 0, -20)
-
-    label5 = QLabel("As this is still in <b>early development state</b>, you need to know if you game is 32 or 64 bit. <i>Later on I will check it on PCGW. For now you will need to select the matter</i>. If you don't know nothing about, check on <a href='https://www.pcgamingwiki.com/wiki/Home'>PCGamingWiki</a>.")
-    label5.setAlignment(Qt.AlignJustify)
-    label5.setMaximumWidth(WINDOW_WIDTH)
-    label5.setWordWrap(True)
-
+    label4 = labelFactory.createLabel("sub_title", "Select the game architecture")
+    label5 = labelFactory.createLabel("body", "As this is still in <b>early development state</b>, you need to know if you game is 32 or 64 bit. <i>Later on I will check it on PCGW. For now you will need to select the matter</i>. If you don't know nothing about, check on <a href='https://www.pcgamingwiki.com/wiki/Home'>PCGamingWiki</a>.")
     bit_32_radio = QRadioButton("32bit")
     bit_64_radio = QRadioButton("64bit")
 
     # Rendering API
-    label6 = QLabel("<font size=5>Select the rendering API</font>")
-    label6.setAlignment(Qt.AlignLeft)
-    label6.setContentsMargins(0, 20, 0, -20)
-
+    label6 = labelFactory.createLabel("sub_title", "Select the rendering API")
     vulkan_radio = QRadioButton("Vulkan")
     d3d9_radio = QRadioButton("DirectX 9c")
     d3d10_radio = QRadioButton("DirectX 10")
 
     # Install
     install_button = QPushButton("Install")
-    # install_button.setFixedSize(WINDOW_WIDTH / 2, WINDOW_WIDTH / 4)
 
     # Draw stuff
     main_layout.addWidget(label1)
