87895ce71de7677320e6cddc233b8f2821efaac3
Ishidawg <willianscagol@gmail.com>
2026-08-03 17:26:19 -0300

    vala env

--- rthook.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from pathlib import Path
-
-base_path = Path(sys._MEIPASS)
-plugins_path = str(base_path / "PySide6" / "Qt" / "plugins")
-
-os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = plugins_path
-os.environ["QT_QPA_PLATFORMTHEME"] = "xdgdesktopportal"
